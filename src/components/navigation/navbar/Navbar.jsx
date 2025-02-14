6b6f2faca2d904a764f9dfbb8d27904a339dd6ec
k1ssa1 <samikissai.edu@gmail.com>
2025-02-14 13:06:50 +0100

    feat: add basic navbar component

--- src/components/navigation/navbar/Navbar.jsx
+++ src/components/navigation/navbar/Navbar.jsx
@@ -1,7 +1,38 @@
+import { Link } from "react-router";
+
+import logo from "../../../assets/logo_transparent.png";
+import styles from "./Navbar.module.css"
+
+import { FaBars } from "react-icons/fa6";
+
 const Navbar = () => {
-    return ( 
-        <>navbar</>
-     );
-}
- 
+  return (
+    <nav className={styles.appbar}>
+      <div className={styles.logo_list}>
+          <Link to="/">
+            <img src={logo} alt="logo" width={120} />
+          </Link>
+          <FaBars color="#291488" size={40} className={styles.hamburger}/>
+      </div>
+      <ul className={styles.anchorlinks_list}>
+        <li className={styles.list_item}>
+            <Link to="#hero">about</Link>
+        </li>
+        <li className={styles.list_item}>
+            <Link to="#services">services</Link>
+        </li>
+        <li className={styles.list_item}>
+            <Link to="#pricing">pricing</Link>
+        </li>
+        <li className={styles.list_item}>
+            <Link to="#pricing">testimonials</Link>
+        </li>
+        <li className={styles.list_item}>
+            <Link to="#contact">contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 export default Navbar;
