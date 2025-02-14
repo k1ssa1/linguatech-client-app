f2403f78d5f2515d3d1f0277f01331c87daa380d
k1ssa1 <samikissai.edu@gmail.com>
2025-02-14 14:04:49 +0100

    feat: add responsivness to navbar

--- src/components/navigation/navbar/Navbar.jsx
+++ src/components/navigation/navbar/Navbar.jsx
@@ -3,18 +3,27 @@ import { Link } from "react-router";
 import logo from "../../../assets/logo_transparent.png";
 import styles from "./Navbar.module.css"
 
-import { FaBars } from "react-icons/fa6";
+import { FaBars, FaEnvelope } from "react-icons/fa6";
+import { useState } from "react";
+import classNames from "classnames";
 
 const Navbar = () => {
+
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const dropdownMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
+
   return (
     <nav className={styles.appbar}>
-      <div className={styles.logo_list}>
+      <div className={classNames(styles.logo_list)}>
           <Link to="/">
             <img src={logo} alt="logo" width={120} />
           </Link>
-          <FaBars color="#291488" size={40} className={styles.hamburger}/>
+          <FaBars color="#291488" size={40} className={styles.barIcon} onClick={dropdownMenu}/>
       </div>
-      <ul className={styles.anchorlinks_list}>
+      <ul className={classNames(styles.anchorlinks_list, { [styles.show]: isMenuOpen})}>
         <li className={styles.list_item}>
             <Link to="#hero">about</Link>
         </li>
@@ -28,7 +37,7 @@ const Navbar = () => {
             <Link to="#pricing">testimonials</Link>
         </li>
         <li className={styles.list_item}>
-            <Link to="#contact">contact</Link>
+            <Link to="#contact"><FaEnvelope size={16}/>contact</Link>
         </li>
       </ul>
     </nav>
