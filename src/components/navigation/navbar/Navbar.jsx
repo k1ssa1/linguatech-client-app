cc3bb7ec1f9653a832452f42de3510b5d3bb8867
k1ssa1 <samikissai.edu@gmail.com>
2025-02-15 02:50:49 +0100

    fix: add appbar positioning and onscroll behavior

--- src/components/navigation/navbar/Navbar.jsx
+++ src/components/navigation/navbar/Navbar.jsx
@@ -4,19 +4,36 @@ import logo from "../../../assets/logo_transparent.png";
 import styles from "./Navbar.module.css"
 
 import { FaBars, FaEnvelope } from "react-icons/fa6";
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import classNames from "classnames";
 
 const Navbar = () => {
 
     const [isMenuOpen, setMenuOpen] = useState(false);
+    const [isScrolled, setIscrolled] = useState(false);
 
     const dropdownMenu = () => {
         setMenuOpen(!isMenuOpen);
     };
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY > 0){
+                setIscrolled(true);
+            }else{
+                setIscrolled(false)
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll)
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, []);
+
   return (
-    <nav className={styles.appbar}>
+    <nav className={classNames(styles.appbar, {[styles.appbar_anchor]: isScrolled})} id="appBar">
       <div className={classNames(styles.logo_list)}>
           <Link to="/">
             <img src={logo} alt="logo" width={120} />
@@ -25,7 +42,7 @@ const Navbar = () => {
       </div>
       <ul className={classNames(styles.anchorlinks_list, { [styles.show]: isMenuOpen})}>
         <li className={styles.list_item}>
-            <Link to="#hero">about</Link>
+            <Link to="#hero">home</Link>
         </li>
         <li className={styles.list_item}>
             <Link to="#services">services</Link>
