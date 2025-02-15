f8bac71699ebe35149ff5d7ef24e23c452b41a6a
k1ssa1 <samikissai.edu@gmail.com>
2025-02-15 01:36:02 +0100

    feat: add animation to pricing section

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useState, useRef, useEffect  } from "react";
 import styles from "./Pricing.module.css"
 
 import { FaCircleCheck  } from "react-icons/fa6";
-
-
+import anime from "animejs/lib/anime.es.js";
 
 const Pricing = () => {
 
     const [isMonthly, setMonthly] = useState(true);
     const [isAnnual, setAnnual] = useState(false);
 
+    const pricingRef = useRef(null);
+    const hasAnimated = useRef(false);
+
+    const pricingTitle = useRef(null);
+    const pricingSubtitle = useRef(null);
+    const pricingDesc = useRef(null);
+    const pricingToggle = useRef(null);
 
     const monthlyPrices = {
         bronze: 19.19,
@@ -33,19 +39,53 @@ const Pricing = () => {
         }
     }
 
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            const entry = entries[0];
+            if (entry.isIntersecting && !hasAnimated.current) {
+              hasAnimated.current = true;
+    
+              anime({
+                targets: `.${styles.card}`,
+                opacity: [0,1],
+                duration: 2000,
+                easing: "easeOutQuad",
+                delay: anime.stagger(1500)
+              })
+
+              anime({
+                targets: [pricingTitle.current, pricingSubtitle.current, pricingDesc.current, pricingToggle.current],
+                opacity: [0,1],
+                duration: 1000,
+                easing: "easeOutQuad",
+                delay: anime.stagger(1000)
+              })
+
+            }
+          },
+          {threshold: 0.3}
+        );
+    
+        if(pricingRef.current){
+            observer.observe(pricingRef.current);
+        }
+    
+        return () => observer.disconnect();
+     }, [])
+
     return ( 
-        <section className={styles.main}>
+        <section ref={pricingRef} className={styles.main}>
             <div className={styles.header}>
-                <h2>Our pricing</h2>
-                <h3>Start your language journey today!</h3>
-                <p>Find the perfect plan to fit your goals and unlock new opportunities with expert led-courses.</p>
-                <div className={styles.btn_group}>
+                <h2 ref={pricingTitle} style={{opacity: 0}}>Our pricing</h2>
+                <h3 ref={pricingSubtitle} style={{opacity: 0}}>Start your language journey today!</h3>
+                <p ref={pricingDesc} style={{opacity: 0}}>Find the perfect plan to fit your goals and unlock new opportunities with expert led-courses.</p>
+                <div ref={pricingToggle} className={styles.btn_group} style={{opacity: 0}}>
                     <button className={isMonthly ? styles.active: styles.inactive} onClick={() => handleToggle("monthly")}>Monthly</button>
                     <button className={isAnnual ? styles.active: styles.inactive} onClick={() => handleToggle("annual")}>Annual</button>
                 </div>
             </div>
             <div className={styles.card_collection}>
-                <div className={styles.card}>
+                <div className={styles.card} style={{opacity: 0}}>
                     <div className={styles.card_head}>
                         <h3>Bronze</h3>
                         <p>${isMonthly ? monthlyPrices.bronze : annualPrices.bronze} / {""} {isMonthly ? "Month" : "Year"}</p>
@@ -60,7 +100,7 @@ const Pricing = () => {
                         </ul>
                     </div>
                 </div>
-                <div className={styles.card}>
+                <div className={styles.card} style={{opacity: 0}}>
                     <div className={styles.card_head}>
                         <h3>Silver</h3>
                         <p>${isMonthly ? monthlyPrices.silver : annualPrices.silver} / {""} {isMonthly ? "Month" : "Year"}</p>
@@ -76,7 +116,7 @@ const Pricing = () => {
                         </ul>
                     </div>
                 </div>
-                <div className={styles.card}>
+                <div className={styles.card} style={{opacity: 0}}>
                     <div className={styles.card_head}>
                         <h3>Gold</h3>
                         <p>${isMonthly ? monthlyPrices.gold : annualPrices.gold} / {""} {isMonthly ? "Month" : "Year"}</p>
